6c98b76f15668715e9885223ca65a94e1939b84a
Derek Butvin <drockscott123@gmail.com>
2022-01-24 13:55:08 -0600

    Created articles controller, index, show, new, create methods, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :articles, only: [:show, :index, :new, :create]
 end
